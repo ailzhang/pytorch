f27be01ca4e994dfd8698cc20b8e0c2f4fde26cd
Ailing <ailzhang@users.noreply.github.com>
2017-08-15 21:45:17 -0700

    fix pytorch.distributed with single GPU on each node

--- torch/nn/parallel/distributed.py
+++ torch/nn/parallel/distributed.py
@@ -150,9 +150,9 @@ class DistributedDataParallel(Module):
         self._start_reduction_threads()
 
     def forward(self, *inputs, **kwargs):
-        if len(self.device_ids) == 1:
-            return self.module(*inputs, **kwargs)
         inputs, kwargs = self.scatter(inputs, kwargs, self.device_ids)
+        if len(self.device_ids) == 1:
+            return self.module(*inputs[0], **kwargs[0])
         self._sync_params()
         outputs = self.parallel_apply(self._module_copies, inputs, kwargs)
         return self.gather(outputs, self.output_device)
